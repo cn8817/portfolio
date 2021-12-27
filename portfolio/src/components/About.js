807cb41cebb31882e631487a27e4384c80375043
cn8817 <christinenguyen1433@gmail.com>
2021-12-26 18:00:15 -0800

    making changes to about section

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,11 +1,22 @@
 import React from "react";
+import linkedin from '../Assets/linkedin.png'
+import github from '../Assets/github.png'
 
 export default function About(){
+
+    const onClickUrl = (url) => {
+        return () => window.open(url, "_blank")
+      }
+      
     return(
         <div className="about-container" id='about'>
             <div className="about-intro">
                 <h1 className="about-h1">Hi, I'm Christine Nguyen</h1>
                 <h2 className="about-h2">I'm a Full Stack Web Developer based in Las Vegas, NV. Frontend Developement is my specialty and creating responsive, user-friendly, and efficient applications is my passion!</h2>
+                <div className="about-social">
+                    <img onClick={onClickUrl('https://github.com/cn8817')} className='about-img' src={github}/>
+                    <img onClick={onClickUrl('https://www.linkedin.com/in/christine8817/')} className='about-img' src={linkedin}/>
+                </div>
             </div>
             <div className="about-skills">
             <div>
